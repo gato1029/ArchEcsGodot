2a18bb88d81986edc1598d78e44475779a3c3637
josue_lap <josue.ccama@gmail.com>
2024-10-25 14:15:30 -0500

    construcccion Base

---
+++ sources/managers/AtackManager.cs
@@ -0,0 +1,196 @@
+﻿using Arch.AOT.SourceGenerator;
+using Arch.Buffer;
+using Arch.Core;
+using Arch.Core.Extensions;
+using Arch.System;
+
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[Component]
+public struct ColliderMelleAtack
+{
+    public Rect2 rect;
+    public Vector2 offset;
+    public Rect2 rectTransform;
+}
+
+[Component]
+public struct RangeAttack
+{
+    public int area;    
+}
+
+[Component]
+public struct FrecuencyAttack
+{
+    public float value;
+    public float timeAccumulator;
+}
+
+[Component]
+public struct PendingAttack
+{
+    public Entity entityTarget; 
+    public int damage;   
+}
+[Component]
+public struct Health
+{
+    public int value;    
+}
+[Component]
+public struct Shield
+{
+    public int value;
+}
+[Component]
+public struct Resistance
+{
+    public int fire;
+    public int water;
+    public int earth;
+    public int air;
+}
+[Component]
+public struct Damage
+{
+    public int value;
+}
+[Component]
+public struct PendingRemove
+{    
+}
+
+internal class AtackManager : BaseSystem<World, float>
+{
+    private CommandBuffer commandBuffer;
+    private QueryDescription query = new QueryDescription().WithAll<Unit, Position, Direction, Melee, ColliderMelleAtack, FrecuencyAttack>();
+    private QueryDescription queryPending = new QueryDescription().WithAll<Unit, Melee, PendingAttack>();
+ 
+    public AtackManager(World world) : base(world)
+    {
+        commandBuffer = new CommandBuffer();
+    }
+
+
+    private readonly struct ProcessJobMelee : IForEachWithEntity<Position, Direction, Damage, ColliderMelleAtack, FrecuencyAttack>
+    {
+        private readonly float _deltaTime;
+        private readonly CommandBuffer _commandBuffer;
+
+        public ProcessJobMelee(float deltaTime, CommandBuffer commandBuffer)
+        {
+            _deltaTime = deltaTime;
+            _commandBuffer = commandBuffer;
+
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Update(Entity entity, ref Position pos, ref Direction d, ref Damage da, ref ColliderMelleAtack ca, ref FrecuencyAttack fa)
+        {
+
+            Vector2 positionAtack = pos.value+ GetRotatedPointByDirection(ca.offset,Vector2.Zero,d.value);
+          
+            var result = CollisionManager.dynamicCollidersEntities.GetPossibleQuadrants(positionAtack, ca.offset.X);            
+            if (result!=null)
+            {
+                foreach (var itemDic in result)
+                {
+                    foreach (var item in itemDic.Value)
+                    {
+                        if (item.Key != entity.Id)
+                        {
+                            Entity entB = item.Value;
+                            if (entB.Has<Sprite>() && !entB.Has<PendingAttack>())
+                            {
+                                var sp = entB.Get<Sprite>();
+                                var entityExternal = entB.Get<Sprite>().rect.Size;
+                                var entityExternalPos = entB.Get<Position>().value;
+
+                                if (CollisionManager.CheckAABBCollision(positionAtack, ca.rect.Size, d.value, entityExternalPos, entityExternal, entB.Get<Direction>().value))
+                                {
+                                    _commandBuffer.Add<PendingAttack>(entity, new PendingAttack { entityTarget = entB, damage = da.value });
+                                }
+                            }
+
+                        }
+                    }
+                }
+               
+            }
+
+
+        }
+
+    }
+
+    private readonly struct ProcessJobPendingAtack : IForEachWithEntity<PendingAttack>
+    {
+        private readonly float _deltaTime;
+        private readonly CommandBuffer _commandBuffer;
+
+        public ProcessJobPendingAtack(float deltaTime, CommandBuffer commandBuffer)
+        {
+            _deltaTime = deltaTime;
+            _commandBuffer = commandBuffer;
+
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Update(Entity entity, ref PendingAttack pa)
+        {
+            pa.entityTarget.Get<Health>().value -= pa.damage; 
+            Health health = pa.entityTarget.Get<Health>();            
+            _commandBuffer.Remove<PendingAttack>(entity);
+            if (!pa.entityTarget.Has<PendingRemove>())
+            {
+                if (health.value <= 0)
+                {
+                    _commandBuffer.Add<PendingRemove>(pa.entityTarget);
+                }
+            }
+            
+        }
+
+    }
+
+  
+
+
+    public static Vector2 GetRotatedPointByDirection(Vector2 point, Vector2 origin, Vector2 direction)
+    {
+        // Normalizar la dirección para obtener un vector unitario
+        direction = direction.Normalized();
+
+        // Calcular el vector perpendicular para el eje de la rotación
+        Vector2 perpDirection = new Vector2(-direction.Y, direction.X);
+
+        // Mover el punto al origen (trasladar)
+        Vector2 relativePoint = point - origin;
+
+        // Aplicar la rotación usando la dirección y su perpendicular
+        Vector2 rotatedPoint = origin + (relativePoint.X * direction + relativePoint.Y * perpDirection);
+
+        return rotatedPoint;
+    }
+
+
+    public override void Update(in float t)
+    {
+        
+        var job = new ProcessJobMelee((float)t, commandBuffer);
+        World.InlineParallelEntityQuery<ProcessJobMelee, Position, Direction, Damage, ColliderMelleAtack, FrecuencyAttack>(in query, ref job);
+        commandBuffer.Playback(World);
+
+        var jobPending = new ProcessJobPendingAtack((float)t, commandBuffer);
+        World.InlineParallelEntityQuery<ProcessJobPendingAtack, PendingAttack>(in queryPending, ref jobPending);
+        commandBuffer.Playback(World);
+    }
+
+
+}
+
---
+++ sources/managers/MovementManager.cs
@@ -0,0 +1,203 @@
+﻿using Arch.AOT.SourceGenerator;
+using Arch.Buffer;
+using Arch.Core;
+using Arch.Core.Extensions;
+using Arch.System;
+
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Godot.HttpRequest;
+
+[Component]
+public struct TargetMovement
+{
+    public Vector2 value;
+    
+}
+[Component]
+public struct Direction
+{
+    public Vector2 value;
+}
+[Component]
+public struct Velocity
+{
+    public float value;
+}
+internal class MovementManager: BaseSystem<World, float>
+{
+    private CommandBuffer commandBuffer;
+    private QueryDescription query = new QueryDescription().WithAll<Position,Velocity, TargetMovement,Direction,Collider, Rotation>();    
+    public MovementManager(World world) : base(world)
+    {
+        commandBuffer = new CommandBuffer();
+    }
+
+    private struct ChunkJobMovement : IChunkJob
+    {
+        private readonly float _deltaTime;
+        private readonly CommandBuffer _commandBuffer;
+
+        public ChunkJobMovement(CommandBuffer commandBuffer, float deltaTime) : this()
+        {
+            _commandBuffer = commandBuffer;
+            _deltaTime = deltaTime;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Execute(ref Chunk chunk)
+        {
+            ref var pointerEntity = ref chunk.Entity(0);
+            ref var pointerPosition = ref chunk.GetFirst<Position>();
+            ref var pointerVelocity = ref chunk.GetFirst<Velocity>();
+            ref var pointerTargetMovement = ref chunk.GetFirst<TargetMovement>();
+            ref var pointerDirection = ref chunk.GetFirst<Direction>();
+            ref var pointerCollider = ref chunk.GetFirst<Collider>();
+            ref var pointerRotation = ref chunk.GetFirst<Rotation>();
+           
+            foreach (var entityIndex in chunk)
+            {
+                ref Entity entity = ref Unsafe.Add(ref pointerEntity, entityIndex);
+                ref Position p = ref Unsafe.Add(ref pointerPosition, entityIndex);
+                ref Velocity v = ref Unsafe.Add(ref pointerVelocity, entityIndex);
+                ref TargetMovement tm = ref Unsafe.Add(ref pointerTargetMovement, entityIndex);
+                ref Direction d = ref Unsafe.Add(ref pointerDirection, entityIndex);
+                ref Collider c = ref Unsafe.Add(ref pointerCollider, entityIndex);
+                ref Rotation r = ref Unsafe.Add(ref pointerRotation, entityIndex);
+
+                Vector2 targetDirection = (tm.value - p.value).Normalized();
+                d.value = targetDirection;
+                Vector2 movement = d.value * v.value * _deltaTime;
+
+                //if (!entity.Has<PendingTransform>() && r.value != Mathf.RadToDeg(targetDirection.Angle()))
+                //{
+                //    r.value = Mathf.RadToDeg(targetDirection.Angle());
+                //    _commandBuffer.Add<PendingTransform>(entity);
+                //}
+                Vector2 movementNext = p.value + movement;
+
+                var entityInternal = c.rect.Size;
+                var resultList = CollisionManager.dynamicCollidersEntities.GetPossibleQuadrants(movementNext, entityInternal.X);
+                bool existCollision = false;
+                foreach (var itemMap in resultList)
+                {
+                    foreach (var item in itemMap.Value)
+                    {
+                        if (item.Key != entity.Id)
+                        {
+                            Entity entB = item.Value;
+                            var entityExternal = entB.Get<Collider>().rect;
+                            var entityExternalPos = entB.Get<Position>().value;
+                            var direction = entB.Get<Direction>().value;
+                            if (CollisionManager.CheckAABBCollision(movementNext, c.rect, entityExternalPos, entityExternal))
+                            {
+                                existCollision = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (existCollision)
+                    {
+                        break;
+                    }
+                }
+
+
+                if (!existCollision)
+                {
+                    // 4. Actualizar la posición de la entidad
+                    p.value += movement;
+
+                    // 5. Verificar si la entidad ha llegado al objetivo
+                    float distanceToTarget = (tm.value - p.value).Length();
+                    if (distanceToTarget <= 1)
+                    {
+                        _commandBuffer.Remove<TargetMovement>(entity);
+                    }
+                }
+
+            }
+        }
+    }
+
+    private readonly struct JobUpdate : IForEachWithEntity<Position, Velocity, TargetMovement, Direction, Collider,Rotation>
+    {
+        private readonly float _deltaTime;
+        private readonly CommandBuffer _commandBuffer;
+
+        public JobUpdate(float deltaTime, CommandBuffer commandBuffer)
+        {
+            _deltaTime = deltaTime;
+            _commandBuffer = commandBuffer;
+
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Update( Entity entity, ref Position p, ref Velocity v, ref TargetMovement tm, ref Direction dir, ref Collider c, ref Rotation r)
+        {
+            
+            Vector2 targetDirection = (tm.value - p.value).Normalized();            
+            dir.value = targetDirection;            
+            Vector2 movement = dir.value * v.value * _deltaTime;
+
+            if (!entity.Has<PendingTransform>() && r.value != Mathf.RadToDeg(dir.value.Angle()))
+            {
+                r.value = Mathf.RadToDeg(dir.value.Angle());
+                _commandBuffer.Add<PendingTransform>(entity);
+            }
+            Vector2 movementNext = p.value + movement;
+     
+            var entityInternal = c.rect.Size;
+            var resultList =CollisionManager.dynamicCollidersEntities.GetPossibleQuadrants(movementNext, entityInternal.X);            
+            bool existCollision = false;
+            foreach (var itemMap in resultList)
+            {
+                foreach (var item in itemMap.Value)
+                {
+                    if (item.Key != entity.Id)
+                    {
+                        Entity entB = item.Value;                        
+                            var entityExternal = entB.Get<Collider>().rect;
+                            var entityExternalPos = entB.Get<Position>().value;
+                            var direction = entB.Get<Direction>().value;
+                            if (CollisionManager.CheckAABBCollision(movementNext, c.rect, entityExternalPos, entityExternal))
+                            {
+                                existCollision = true;
+                                break;
+                            }                      
+                    }
+                }
+
+            }
+            
+            
+            if (!existCollision)
+            {
+                // 4. Actualizar la posición de la entidad
+                p.value += movement;
+
+                // 5. Verificar si la entidad ha llegado al objetivo
+                float distanceToTarget = (tm.value - p.value).Length();
+                if (distanceToTarget <= 1)
+                {
+                    _commandBuffer.Remove<TargetMovement>(entity);
+                }
+            }
+          
+        }
+
+    }
+
+    public override void Update(in float t)
+    {
+        
+        //var job = new JobUpdate((float)t, commandBuffer);
+        //World.InlineParallelEntityQuery<JobUpdate,Position, Velocity, TargetMovement, Direction, Collider,Rotation>(in query, ref job);
+        World.InlineParallelChunkQuery(in query, new ChunkJobMovement(commandBuffer, t));
+        commandBuffer.Playback(World);
+    }
+}
+
